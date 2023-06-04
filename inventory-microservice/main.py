ef8228b99164d13cdcfe9c701cb221e9bdeae02f
yanliu1111 <liuyan0504@gmail.com>
2023-06-04 00:53:34 -0600

    feat: CRUD done in inventory micro

--- inventory-microservice/main.py
+++ inventory-microservice/main.py
@@ -1,9 +1,17 @@
 from fastapi import FastAPI
+from fastapi.middleware.cors import CORSMiddleware
 from redis_om import get_redis_connection, HashModel
 from dotenv import load_dotenv
 import os
 load_dotenv()  # Load environment variables from the .env file
 app = FastAPI()
+
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=["http://localhost:3000"],
+    allow_methods=["*"],
+    allow_headers=["*"],
+)
 
 redis = get_redis_connection(
     host=os.getenv("REDIS_HOST"),
@@ -22,4 +30,26 @@ class Product(HashModel):
 
 @app.get("/products")
 def all():
-    return Product.all_pks()
+    return [format(pk) for pk in Product.all_pks()] #all_pks() returns all the primary keys of the products
+
+# Format function to return the product in the format we want
+def format(pk: str):
+    product = Product.get(pk)
+    return {
+        "id": product.pk,
+        "name": product.name,
+        "price": product.price,
+        "quantity": product.quantity
+    }
+
+@app.post('/products')
+def create(product: Product):
+    return product.save()
+
+@app.get('/products/{pk}')
+def get(pk: str):
+    return Product.get(pk)
+
+@app.delete('/products/{pk}')
+def delete(pk: str):
+    return Product.delete(pk)
