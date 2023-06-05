e931dceceb95ca4692e9855b5fc42aa22389d8f1
yanliu1111 <liuyan0504@gmail.com>
2023-06-04 18:56:48 -0600

    feat: order-microservice setup w 1st http request

---
+++ payment-microservice/main.py
@@ -0,0 +1,44 @@
+from fastapi import FastAPI
+from fastapi.middleware.cors import CORSMiddleware
+from redis_om import get_redis_connection, HashModel
+from dotenv import load_dotenv
+from starlette.requests import Request
+import requests
+
+import os
+load_dotenv()  # Load environment variables from the .env file
+app = FastAPI()
+
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=["http://localhost:3000"],
+    allow_methods=["*"],
+    allow_headers=["*"],
+)
+# This SHOULD be a different database than the one used by the inventory microservice
+# But in this test, I am using the same database as the inventory microservice
+redis = get_redis_connection(
+    host=os.getenv("REDIS_HOST"),
+    port=os.getenv("REDIS_PORT"),
+    password=os.getenv("REDIS_PASSWORD"),
+    decode_responses=True
+)
+
+class Order(HashModel):
+    product_id: str
+    price: float
+    fee: float
+    total: float
+    quantity: int
+    status: str # pending, completed, refunded
+
+    class Meta:
+        database = redis
+    
+    @app.post('/orders')
+    async def create(request: Request): # id, quantity
+        body = await request.json()
+
+        req = requests.get(f'http://localhost:8000/products/%s' % body['id'])
+        # converted: %s to % body['id']
+        return req.json()
