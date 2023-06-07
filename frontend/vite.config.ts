1eda6b82bddba055b366ad1c9ca45d813e5f6c92
yanliu1111 <liuyan0504@gmail.com>
2023-06-07 00:03:10 -0600

    feat: one more proxy for other microservice

--- frontend/vite.config.ts
+++ frontend/vite.config.ts
@@ -11,6 +11,11 @@ export default defineConfig({
         changeOrigin: true,
         rewrite: (path) => path.replace(/^\/api/, ""),
       },
+      "/app": {
+        target: "http://127.0.0.1:8001",
+        changeOrigin: true,
+        rewrite: (path) => path.replace(/^\/app/, ""),
+      },
     },
   },
 });
